8a42c5e032d0efacb627d2e0fec7e1a8f4cd0b81
Rohin Adalja <rohin.adalja@mail.utoronto.ca>
2020-02-04 18:57:15 -0500

    Add gatsby plugin for theme logic :white_check_mark:

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  plugins: 
+  [
+    {
+      resolve: "gatsby-theme-portfolio",
+      options: 
+      {
+        contentPath: `${__dirname}/content`
+      },
+    },
+  ],
+
+};
